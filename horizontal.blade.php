15986368e23bf883a667cd704c8483a65c617198
AlexBarma <consultantbar93@gmail.com>
2023-04-02 22:02:25 +0200

    Не правильный коммит
    Исправил строчку комменария

--- horizontal.blade.php
+++ horizontal.blade.php
@@ -1,4 +1,4 @@
-//путь C:\OSPanel\domains\crocodeal.org\resources\views\layouts\inc\social\horizontal.blade.php
+<!-- путь C:\OSPanel\domains\crocodeal.org\resources\views\layouts\inc\social\horizontal.blade.php -->
 <div class="text-center mt-4 mb-4 ms-0 me-0">
     <p>Поделитесь вашим объявлением в соцсетях</p>
     <button class='btn btn-fb share s_facebook'><i class="fab fa-facebook"></i> </button>&nbsp;
