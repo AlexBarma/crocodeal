4a8572200e3c4764716b55c5adc633a85e13e13a
AlexBarma <consultantbar93@gmail.com>
2023-04-02 21:57:12 +0200

    Добавил поясняющий заголовок
    Добавил поясняющий заголовок "Поделитесь вашим объявлением в соцсетях" над иконками в объявлении

---
+++ horizontal.blade.php
@@ -0,0 +1,7 @@
+//путь C:\OSPanel\domains\crocodeal.org\resources\views\layouts\inc\social\horizontal.blade.php
+<div class="text-center mt-4 mb-4 ms-0 me-0">
+    <p>Поделитесь вашим объявлением в соцсетях</p>
+    <button class='btn btn-fb share s_facebook'><i class="fab fa-facebook"></i> </button>&nbsp;
+    <button class='btn btn-fb share s_instagram'><i class="fab fa-instagram"></i> </button>&nbsp;
+    <button class='btn btn-fb share s_telegram'><i class="fab bi bi-telegram"></i> </button>&nbsp;
+</div>
