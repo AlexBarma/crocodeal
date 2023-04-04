d68e4ff37d7473daddf28135fc0823f2250479a0
AlexBarma <consultantbar93@gmail.com>
2023-04-04 17:11:50 +0200

    Поделитесь в соцсетях
    Исправил текст по смыслу "Поделитесь в соцсетях"

--- horizontal.blade.php
+++ horizontal.blade.php
@@ -1,6 +1,6 @@
 <!-- путь C:\OSPanel\domains\crocodeal.org\resources\views\layouts\inc\social\horizontal.blade.php -->
 <div class="text-center mt-4 mb-4 ms-0 me-0">
-    <p>Поделитесь вашим объявлением в соцсетях</p>
+    <p>Поделитесь в соцсетях</p>
     <button class='btn btn-fb share s_facebook'><i class="fab fa-facebook"></i> </button>&nbsp;
     <button class='btn btn-fb share s_vk' type='button'>vk<span class='badge counter c_vk'></span></button>&nbsp;
     <button class='btn btn-wa share s_whatsapp'><i class="fab fa-whatsapp"></i> </button>&nbsp;
