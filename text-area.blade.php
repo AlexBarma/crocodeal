80c1420a5ac49492961367a5baba23f09e3bb919
AlexBarma <129320193+AlexBarma@users.noreply.github.com>
2023-04-27 23:16:54 +0300

    Созданы блок ютьюб и полезная информация
    В коде text-area  Созданы блок ютьюб и полезная информация.

--- text-area.blade.php
+++ text-area.blade.php
@@ -29,27 +29,19 @@ $hideOnMobile = (data_get($sectionOptions, 'hide_on_mobile') == '1') ? ' hidden-
 	<div class="card">
 		@if (!empty($textTitle))
 		<h2 class="card-title">{{ $textTitle }}</h2>
+		<!--Block YouTube info block-->
+		<div style="display: grid; grid-gap: 15px; grid-template-columns: repeat(auto-fit, minmax(320px, 1fr)); grid-template-rows: repeat(100%);"><!-- YouTube block end -->
+			<p><iframe style="padding: 15px; border-radius: 30px;" title="YouTube video player" src="https://www.youtube.com/embed/t0KEW8ekFDY" width="100%" height="500" frameborder="0" allowfullscreen="">
+				</iframe></p>
+			<!-- info block start -->
+			<div style="width: 100%; padding: 15px;; text-decoration: none;">
+				<div class="info" style="width: 100%; height: 470px; border-radius: 20px; margine-top: 35px; text-decoration: none; background: linear-gradient(PaleTurquoise, #4682b4); display: flex; justify-content: center; align-items: center;">&nbsp; <span style="font-size: 18pt;"><strong><a style="color: #f8f9fa; display: block; margin: 0 auto; padding: 56px; text-align: center;" href="http://crocodeal.test/page/Egypt%20news%20block"><h1>{{ t('helpful_information') }}</h1></a></strong></span></div>
+			</div>
+		</div>
+		<!-- info block end --> 
 		@endif
 		@if (!empty($textBody))
 		<div>{!! $textBody !!}</div>
-		
-		<!--Block YouTube-->
-		<div 
-		style="display: grid;
-		 grid-gap: 0px;
-		  grid-template-columns: repeat(auto-fit, minmax(320px, 1fr));
-		   grid-template-rows: repeat(100%);">
-			<p>
-				<iframe 
-				style="padding-top: 15px;
-				  border-radius: 20px;
-				 " title="YouTube video player" src="https://www.youtube.com/embed/t0KEW8ekFDY" width="100%" height="500" frameborder="0" allowfullscreen=""></iframe></p>
-			<!-- info block end -->
-			<!--Блок конвертер валюты-->
-			<div style="width:100%;padding-top: 15px;"><fxwidget-cc amount="100" decimals="2" large="false" shadow="true" symbol="true" grouping="true" border="true" from="USD" to="EUR" background-color="#4682b4"></fxwidget-cc><a href="https://currencyrate.today/">CurrencyRate</a>
-			<script async src="https://s.fx-w.io/widgets/currency-converter/latest.js"></script></div>
-			<!-- info block end -->
-		</div>
 		@endif
 	</div>
 </div>
